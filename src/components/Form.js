0287f277113483ced67a7f955c701b55150ce8db
Alvin Motieram <amotieram99@yahoo.com>
2020-10-08 23:26:17 -0400

    Think I got it

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,54 +3,62 @@ import React, {Component} from 'react';
 class Form extends Component {
     constructor(props) {
         super(props);
-        
-        this.state = {
-            name: "",
-            URL: ""
-        }
+        this.state ={
+            name: "", 
+            url: ""
+        } 
+        /*
+            TODO - set initial state for link name and URL 
+        */
+       this.handleChange = this.handleChange.bind(this); 
+       this.onFormSubmit = this.onFormSubmit.bind(this);
     }
+
+    handleChange = event => {
+        /*
+            TODO - Logic for changing state based on form changes
+        */
+       
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
     
-    handleNameChange = event => {
-        /*
-            TODO - Logic for changing state based on form changes
-        */
-       console.log("input updated")
-       console.log(event.target)
-
-       this.setState({name: event.target.value})
-    }
-
-    handleURLChange = event => {
-        /*
-            TODO - Logic for changing state based on form changes
-        */
-       console.log("input updated")
-       console.log(event.target)
-
-       this.setState({URL: event.target.value})
+        this.setState({
+          [name]: value
+        });
     }
 
     onFormSubmit = (event) => {
         // to prevent page reload on form submit
         event.preventDefault();
         
-        console.log(this.state)
-
-        this.props.handleSubmit(this.state)
-
+        /*
+            TODO - Logic for calling props to handle submission and setting state changes
+        */
+        let name = this.state.name;
+        let url = this.state.url;
+        let obj = {name, url};
+        console.log("obj", obj)
+        this.props.handleSubmit(obj);
+        this.setState({
+            Name: "", 
+            URL: ""
+          });
     }
 
     render() {
-
+       console.log("Name", this.state.name);
+       console.log("URL", this.state.url);
         return(
             <form>
-                <label>Name</label>
-                <input type="text" name="name" onChange={this.handleNameChange} value={this.state.name}></input>
-                <label>URL</label>
-                <input type="text" name="URL" onChange={this.handleURLChange} value={this.state.URL}></input>
-                <button onClick={this.onFormSubmit}> Submit </button>
+                {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+                <label for="name">Name:</label><br/>
+                <input type="text" id="name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
+                <label for="url">URL:</label><br/>
+                <input type="text" id="url" name="url" value={this.state.url} onChange={this.handleChange}/><br/><br/>
+                <input type="submit" value="Submit" onClick = {this.onFormSubmit}/>
+                
             </form>
-
         );
     
     }
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -7,22 +7,32 @@ class LinkContainer extends React.Component {
         super(props)
         /* TODO - Create state object for storing favLinks */
         this.state ={
-            favLinks: []
+            favLink: []
         }
+        this.removeCharacter = this.removeCharacter.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
 
     removeCharacter = index => {
+        console.log("what is, ",index);
         /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
+       this.setState((prevState) => ({
+   
+       favLink: prevState.favLink.filter((_, i) => i !== index)
+     }));
     }
 
     handleSubmit = favLink => {
+        
         /*
             TODO - Create logic to setState and add new favLink to favLinks array in state
         */
-        this.setState({ favLinks: this.state.favLinks.concat(favLink)})
+       // this.setState({favLink:this.state.favLink.push(favLink)});
+       this.setState( prevState => ({
+        favLink: [...prevState.favLink, favLink]
+    }));
     }
 
     render() {
@@ -32,12 +42,14 @@ class LinkContainer extends React.Component {
                 <h1>My Favorite Links</h1>
                 <p>Add a new url with a name and link to the table.</p>
                 {/*TODO - Add Table Component */}
-                    <Table linkData={this.state.favLinks} />
-                <br/>
+                <Table removeLink={this.removeCharacter} linkData = {this.state.favLink}/> 
                 
+                <br/>
+
                 <h3>Add New</h3>
                 {/*TODO - Add Form Component */}
-                    <Form handleSubmit={this.handleSubmit} />
+                <Form handleSubmit = {this.handleSubmit}/>
+                
             </div>
         );
     }
